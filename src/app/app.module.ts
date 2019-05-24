5c1b719c87b467eb5df2cf43f3045ff3a856b670
iam4570 <iam4570@yahoo.co.jp>
2019-05-24 21:21:12 +0900

    fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -21,7 +21,7 @@ import { FileService } from '../service/file.service';
 import { OnsenModule } from 'ngx-onsenui';
 import { CDVFile } from '../pipe/cdvfile.pipe';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-import { OrderByPipe } from './descending-order';
+import { OrderByPipe } from '../pipe/descending-order';
 
 const components = [
     AppComponent,
